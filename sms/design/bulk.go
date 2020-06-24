ddea4c6587d363ef3c06adb66c410b48600bffed
William Ondenge <ondengew@gmail.com>
2020-06-24 14:19:34 +0300

    API design

--- sms/design/bulk.go
+++ sms/design/bulk.go
@@ -10,17 +10,6 @@ import (
 var BulkPayload = Type("BulkPayload", func() {
 	Description("Send Bulk SMS through your application")
 
-	Attribute("apiKey", String, func() {
-		Description("Africa’s Talking application apiKey")
-	})
-	Attribute("Content-Type", String, func() {
-		Description("The requests content type")
-		Default("application/x-www-form-urlencoded")
-	})
-	Attribute("Accept", String, func() {
-		Description("The requests response type.")
-		Default("application/json")
-	})
 	Attribute("username", String, func() {
 		Description("Africa’s Talking application username")
 	})
@@ -65,15 +54,17 @@ var BulkPayload = Type("BulkPayload", func() {
 		Description("The keyword to be used for a premium service")
 	})
 	Attribute("linkId", String, func() {
+
 		// AT forwards the linkId to our application
 		// when the user sends a message to our service.
 		Description("Used for premium services to send OnDemand messages")
 	})
 	Attribute("retryDurationInHours", String, func() {
+
 		// In case it’s not delivered to the subscriber
 		Description("No. of hours subscription message should be retried")
 	})
-	Required("apiKey", "Content-Type", "username", "to", "message")
+	Required("username", "to", "message")
 })
 
 var BulkResponse = ResultType("BulkResponse", func() {
@@ -86,8 +77,9 @@ var BulkResponse = ResultType("BulkResponse", func() {
 			Description("A Map detailing the eventual result of the sms request")
 			Attribute("Message", String, func() {
 				Description("Summary of recipients sms was sent to and the total cost incurred.")
+				Example("Sent to 1/1 Total Cost: KES 0.8000")
 			})
-			Attribute("Recipients", ArrayOf(Recipient))
+			Attribute("Recipients", CollectionOf(Recipient))
 		})
 	})
 	View("default", func() {
