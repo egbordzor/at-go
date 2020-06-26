2de67083822ec4f6c93463cbbe574c4c14df00bc
William Ondenge <ondengew@gmail.com>
2020-06-26 11:30:56 +0300

    SDK Updates.

---
+++ query.go
@@ -0,0 +1,15 @@
+package atgo
+
+const (
+	FindTransactionLiveURL    = "https://payments.africastalking.com/query/transaction/find"
+	FindTransactionSandboxURL = "https://payments.sandbox.africastalking.com/query/transaction/find"
+
+	FetchProductTransactionsLiveURL    = "https://payments.africastalking.com/query/transaction/fetch"
+	FetchProductTransactionsSandboxURL = "https://payments.sandbox.africastalking.com/query/transaction/fetch"
+
+	FetchWalletTransactionsLiveURL    = "https://payments.africastalking.com/query/wallet/fetch"
+	FetchWalletTransactionsSandboxURL = "https://payments.sandbox.africastalking.com/query/wallet/fetch"
+
+	FetchWalletBalanceLiveURL    = "https://payments.africastalking.com/query/wallet/balance"
+	FetchWalletBalanceSandboxURL = "https://payments.sandbox.africastalking.com/query/wallet/balance"
+)
